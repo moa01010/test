21c1a73bda9ed60065ba42443490d554c07adcc6
moa01010 <33689033+moa01010@users.noreply.github.com>
2017-11-29 05:23:33 +0900

    最初の方できた

--- movie_test.js
+++ movie_test.js
@@ -1,13 +1,3 @@
-//var tar = document.getElementById("bgvid");
-//document.getElementById('bgvid') = "tar";
-/*
-if(document.getElementById("bgvid") == null){ //もしIDの取得に失敗したら
-	alert("tar is null");
-} else {
-	alert("tar not null");
-}
-*/
-
 /* 終わったら動画を入れ替えるやつ
 var movie_url = "SMS_test.mp4";
 tar.addEventListener('ended', function(){
@@ -17,64 +7,70 @@ tar.addEventListener('ended', function(){
 })
 */
 
-function koshin(){
-	document.write("<p>JavaScript Koshin</p>");
-	//document.div.innerHTML += "<p>JavaScript Test</p>";
-	var tar = document.getElementById("bgvid");
-	if(document.getElementById("bgvid") == null){ //もしIDの取得に失敗したら
-		alert("tar is null");
-	} else {
-		alert("tar not null");
-	}
-
-	setTimeout(tar.pause(), 3000);
-
-	tar.addEventListener("timeupdate", function(){
-		if(media.currentTime > 5){
-			document.write("<p>ok</p>");
-		}
-	})
-	/*
-	for(var n = 0;tar.currentTime > 0;){
-		document.write("<p>1ok</p>");
-
-		//参考 https://teratail.com/questions/11111
-		if (tar.currentTime > 3){
-			document.write("<p>2ok</p>");
-			satouHyouji();
-		}
-
-	}
-	*/
-	
-}
-
+/*
 //再生
 function play(){
-	tar.play();
+	m.play();
 }
 
 //一時停止
 function pause(){
-	//document.write("<p>pause</p>");
-	tar.pause();
+	m.pause();
 }
 
 //時間指定
-function go(seconds){
-	tar.currentTime =seconds;
+function go(var seconds){
+	m.currentTime = seconds;
+}
+*/
+
+var m = 0;
+var p1 = 0;
+
+function koshin(){
+	m = document.getElementById("bgvid");
+	p1 = document.getElementById("point1");
+
+	/* m関連付け成功してるか確認用
+	if(m == 0 || m == null){ //もしIDの取得に失敗したら
+		alert("m is 0");
+	} else {
+		alert("m is not 0");
+	}
+	*/
+
+	//佐藤のメッセージクリックさせる
+	m.addEventListener("timeupdate", satouHyouji, false);
 }
 
 
 function satouHyouji(){
-	var tar = document.getElementById("bgvid");
-	document.write("<img src'icon_hand.png' id='makerMes'>");
-	pause();
-	//setTimeout(document.write("<img src='icon_hand.png' id='makerMes'>"), 5000);
-	//pause();
+	if(m.currentTime > 10){
+		m.pause();
+		p1.style.visibility = "visible";
+		p1.innerHTML = "<img src='image/icon_hand.png' class='makerMes'>";
+		
+		p1.addEventListener("click", next1, false);
+
+		m.removeEventListener("timeupdate", satouHyouji, false);
+	}
+
+	m.addEventListener("timeupdate", patt1, false);
+}
+
+function next1(){
+	m.play();
+	p1.style.visibility = "hidden";
+}
+
+function patt1(){
+
 }
 
 
 
-//setTimeout(pause, 3000);
+
+
+
+
 
